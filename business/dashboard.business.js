b789eb291314f20d4378ebb3d2c39a6d6860ad2f
Jesvir Subang <jesvirxyz@gmail.com>
2020-11-04 15:32:02 +0800

    Second COmmint

--- business/dashboard.business.js
+++ business/dashboard.business.js
@@ -1,37 +1,49 @@
-const { result, update } = require('lodash');
+const { result, update, isEmpty } = require('lodash');
 const { msg } = require('../../../../../config/messages'),
 	{
 		pickAppointmentResponse,
+		pickDashboardCategories,
 	} = require('../../../../helpers/pickResponse.helper'),
 	{ User } = require('../../user/models/user.model'),
 	{ Appointment } = require('../../appointments/models/appointments.model'),
 	{ Client } = require('../../client/models/client.model'),
-	{ Location } = require('../../location/models/location.model');
+	{ Location } = require('../../location/models/location.model'),
+	moment = require('moment');
 
 exports.compareNewAndReturn = async (data) => {
-	let list = data.query.list;
+	let body = pickDashboardCategories(data.body);
+
+	// let location = ['5f5f3e8782303800122d02f7', '5f6ee433e365e80019f5fe8a'];
+	// let location = ['5f5f3e8782303800122d02f7'];
+	// let start_date = '2020-10-29T05:30:00.000Z';
+	// let end_date = '2020-10-30T05:30:00.000Z';
+	let location = body.locationId;
+	let start_date = body.start_date;
+	let end_date = body.end_date;
+	let ownerId = mongoose.Types.ObjectId(data.owner);
 	let finalResult;
 	try {
-		let location = await Location.findById(data.query.location);
 		let results, results2;
-		let selectedDate = new Date(data.query.date);
-		let month = selectedDate.getMonth();
-		let year = selectedDate.getFullYear();
 
+		let startOfYear = moment(start_date).startOf('year');
+		let endOfYear = moment(end_date).endOf('year');
+		let startOfWeek = moment(start_date).startOf('week');
+		let endOfWeek = moment(end_date).endOf('week');
+		let startOfMonth = moment(start_date).startOf('month');
+		let endOfMonth = moment(end_date).endOf('month');
 		let queryArr, queryArr2;
-		if (!year || !month) {
-			console.log('No Month and Year');
-			finalResult = [];
-		} else {
+		let finalReturn = 0,
+			finalNew = 0;
+		for (let i = 0; i < location.length; i++) {
 			if (data.query.month) {
 				queryArr = [
 					{
 						$match: {
-							location: mongoose.Types.ObjectId(location._id),
+							location: mongoose.Types.ObjectId(location[i]),
 							checkout: true,
 							createdAt: {
-								$gte: new Date(year, month, 1),
-								$lte: new Date(year, month + 1, 0),
+								$gte: new Date(startOfMonth),
+								$lte: new Date(endOfMonth),
 							},
 							status: 'completed',
 							client: { $ne: null },
@@ -57,7 +69,7 @@ exports.compareNewAndReturn = async (data) => {
 						$match: {
 							return_count: { $gt: 1 },
 							clientReg: {
-								$lt: [new Date(year, month, 1, 0, 0, 0)],
+								$lt: [new Date(startOfMonth)],
 							},
 						},
 					},
@@ -69,11 +81,11 @@ exports.compareNewAndReturn = async (data) => {
 				queryArr2 = [
 					{
 						$match: {
-							location: mongoose.Types.ObjectId(location._id),
+							location: mongoose.Types.ObjectId(location[i]),
 							checkout: true,
 							createdAt: {
-								$gte: new Date(year, month, 1),
-								$lte: new Date(year, month + 1, 0),
+								$gte: new Date(startOfMonth),
+								$lte: new Date(endOfMonth),
 							},
 							status: 'completed',
 							client: { $ne: null },
@@ -99,7 +111,7 @@ exports.compareNewAndReturn = async (data) => {
 						$match: {
 							return_count: { $lte: 1 },
 							clientReg: {
-								$gte: [new Date(year, month, 1)],
+								$gte: [new Date(startOfMonth)],
 							},
 						},
 					},
@@ -112,11 +124,11 @@ exports.compareNewAndReturn = async (data) => {
 				queryArr = [
 					{
 						$match: {
-							location: mongoose.Types.ObjectId(location._id),
+							location: mongoose.Types.ObjectId(location[i]),
 							checkout: true,
 							createdAt: {
-								$gte: new Date(year, 0),
-								$lte: new Date(year, 11, 31),
+								$gte: new Date(startOfYear),
+								$lte: new Date(endOfYear),
 							},
 							status: 'completed',
 							client: { $ne: null },
@@ -142,7 +154,7 @@ exports.compareNewAndReturn = async (data) => {
 						$match: {
 							return_count: { $gte: 1 },
 							clientReg: {
-								$lt: [[new Date(year, 0, 1, 0, 0, 0)]],
+								$lt: [[new Date(startOfYear)]],
 							},
 						},
 					},
@@ -154,11 +166,11 @@ exports.compareNewAndReturn = async (data) => {
 				queryArr2 = [
 					{
 						$match: {
-							location: mongoose.Types.ObjectId(location._id),
+							location: mongoose.Types.ObjectId(location[i]),
 							checkout: true,
 							createdAt: {
-								$gte: new Date(year, 0),
-								$lte: new Date(year, 11, 31),
+								$gte: new Date(startOfYear),
+								$lte: new Date(endOfYear),
 							},
 							status: 'completed',
 							client: { $ne: null },
@@ -184,8 +196,94 @@ exports.compareNewAndReturn = async (data) => {
 						$match: {
 							return_count: { $gte: 1 },
 							clientReg: {
-								$gte: [new Date(year, 0, 1, 0, 0, 0)],
-								$lt: [[new Date(year, 11, 31, 0, 0, 0)]],
+								$gte: [new Date(startOfYear)],
+								$lt: [[new Date(endOfYear)]],
+							},
+						},
+					},
+					{
+						$count: 'new',
+					},
+				];
+			}
+			if (data.query.week) {
+				queryArr = [
+					{
+						$match: {
+							location: mongoose.Types.ObjectId(location[i]),
+							checkout: true,
+							createdAt: {
+								$gte: new Date(startOfWeek),
+								$lte: new Date(endOfWeek),
+							},
+							status: 'completed',
+							client: { $ne: null },
+						},
+					},
+					{
+						$lookup: {
+							from: 'clients',
+							localField: 'client',
+							foreignField: '_id',
+							as: 'clients',
+						},
+					},
+
+					{
+						$group: {
+							_id: '$client',
+							clientReg: { $addToSet: '$clients.createdAt' },
+							return_count: { $sum: 1 },
+						},
+					},
+					{
+						$match: {
+							return_count: { $gte: 1 },
+							clientReg: {
+								$lt: [[new Date(startOfWeek)]],
+							},
+						},
+					},
+					{
+						$count: 'returned',
+					},
+				];
+
+				queryArr2 = [
+					{
+						$match: {
+							location: mongoose.Types.ObjectId(location[i]),
+							checkout: true,
+							createdAt: {
+								$gte: new Date(startOfWeek),
+								$lte: new Date(endOfWeek),
+							},
+							status: 'completed',
+							client: { $ne: null },
+						},
+					},
+					{
+						$lookup: {
+							from: 'clients',
+							localField: 'client',
+							foreignField: '_id',
+							as: 'clients',
+						},
+					},
+
+					{
+						$group: {
+							_id: '$client',
+							clientReg: { $addToSet: '$clients.createdAt' },
+							return_count: { $sum: 1 },
+						},
+					},
+					{
+						$match: {
+							return_count: { $gte: 1 },
+							clientReg: {
+								$gte: [new Date(startOfWeek)],
+								$lt: [[new Date(endOfWeek)]],
 							},
 						},
 					},
@@ -201,21 +299,30 @@ exports.compareNewAndReturn = async (data) => {
 			let appointment2 = await Appointment.aggregate(
 				queryArr2
 			).allowDiskUse(true);
-			results = appointment[0].returned;
-			results2 = appointment2[0].new;
+			if (appointment.length > 0) {
+				results = appointment[0].returned;
+			} else {
+				results = 0;
+			}
+			if (appointment2.length > 0) {
+				results2 = appointment2[0].new;
+			} else {
+				results2 = 0;
+			}
+			finalReturn = finalReturn + results;
+			finalNew = finalNew + results2;
 
 			finalResult = [
 				{
-					returned: results,
-					new: results2,
+					returned: finalReturn,
+					new: finalNew,
 				},
 			];
 		}
 
-		return (list && pickAppointmentResponse(finalResult)) || finalResult;
+		return finalResult;
 	} catch (error) {
-		finalResult = [];
-		return (list && pickAppointmentResponse(finalResult)) || finalResult;
+		throw error;
 	}
 };
 
@@ -230,3 +337,4 @@ exports.compareNewAndReturn = async (data) => {
 // 		throw error;
 // 	}
 // };
+
--- routes/dashboard.routes.js
+++ routes/dashboard.routes.js
@@ -6,7 +6,8 @@ const { Router } = require('express'),
 	} = require('../controllers/dashboard.controllers');
 const { authenticate } = require('../../../../middleware/jwt.middleware');
 
-router.get('', all_appointments);
+router.post('', all_appointments);
 // router.patch('/:id', update_client);
 
 module.exports = router;
+
